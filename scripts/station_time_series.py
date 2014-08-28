d46032ab2891201da0e315167df21c40d8cece4f
Dan Calacci <dan@dcalacci.net>
2014-08-28 08:11:29 -0400

    updated script

--- scripts/station_time_series.py
+++ scripts/station_time_series.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import pandas_utils
 
 stations = pd.read_csv('../data/stations_10_12_to_11_13.csv', 
                        index_col=0)
@@ -19,13 +20,17 @@ def get_trips_for_date(d):
     start_trips = trips[trips.start_date == d].start_station
     end_trips = trips[trips.end_date == d].end_station
     return {'starting': list(start_trips),
-            'ending': list(end_trips)}    
+            'ending': list(end_trips)}
 
 
 start_date = sorted(trips.start_date)[0]
 end_date = sorted(trips.end_date)[-1]
 timerange = pd.date_range(start_date, end_date, freq='min')
-starting_ending = pd.DataFrame(timerange.map(get_trips_for_date), index=timerange)
+
+
+
+
+starting_ending = timerange.map(get_trips_for_date)
 
 starting_ending.to_csv('trip_timeseries.csv',
 					   sep="\t")
